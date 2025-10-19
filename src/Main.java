5abb097355a8759d21dbaaa119209a5a2c776e1a
Jaimewww <jaime.landazuri@unl.edu.ec>
2025-10-18 19:54:35 -0500

    update 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,101 @@
+import dominio.EstadoTicket;
 import dominio.Nota;
 import dominio.Ticket;
+import estructuras.Node;
 import estructuras.Queue;
 import estructuras.SimpleList;
+import estructuras.Stack;
+
+import java.util.Scanner;
 
 public class Main {
+    static Stack<Ticket> ticketStack = new Stack<>();
+    static Queue<Ticket> ticketQueue = new Queue<>();
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Queue<Ticket> ticketQueue = new Queue<>();
-        Ticket newTicket = new Ticket(1, null);
-        Ticket ticket1 = new Ticket(5, null);
-        ticketQueue.enqueue(newTicket);
-        Nota newNota = ticket1.agregarNota("wilson");
-        SimpleList<Nota> historial = ticket1.getHistorialNotas();
-        System.out.println(historial.find(newNota).toString());
+        handleMenuOption(1);
+        handleMenuOption(2);
+        /*
+            *************************************************
+            ***     Sistema de Gestión de Casos - CAE     ***
+            *************************************************
+            --- MENÚ PRINCIPAL ---
+            1. Recibir nuevo caso
+            2. Atender siguiente caso
+            3. Ver casos en espera
+            4. Consultar historial de caso (por ID)
+            0. Salir
+            Seleccione una opción: 1
+            Nombre del estudiante: Pedro Torres
+            Tipo de trámite: Retiro de Asignatura
+            >> Caso 4 recibido y encolado. <<
+
+            --- MENÚ PRINCIPAL ---
+            ...
+            Seleccione una opción: 3
+            --- Casos en Espera (4) ---
+            Frente -> [ID 1] -> [ID 2] -> [ID 3] -> [ID 4] -> Fin
+         */
     }
+
+    public static void handleMenuOption(int option) {
+        switch (option) {
+            case 1:
+                recibirNuevoCaso();
+                break;
+            case 2:
+                atenderSiguienteCaso();
+                break;
+            case 3:
+                // Lógica para ver casos en espera
+                break;
+            case 4:
+                // Lógica para consultar historial de caso por ID
+                break;
+            case 0:
+                System.out.println("Saliendo del sistema. ¡Hasta luego!");
+                break;
+            default:
+                System.out.println("Opción inválida. Por favor, intente de nuevo.");
+        }
+    }
+
+    public static void recibirNuevoCaso(){
+        System.out.println("Ingrese el nombre del estudiante:");
+        String nombreEstudiante = sc.nextLine();
+        System.out.println("Ingrese el tipo de trámite:");
+        String tipoTramite = sc.nextLine();
+        Ticket nuevoTicket = new Ticket(nombreEstudiante, tipoTramite);
+        int nuevoId = ticketQueue.size() + ticketStack.size() + 1;
+        nuevoTicket.setId(nuevoId);
+        ticketQueue.enqueue(nuevoTicket);
+        System.out.println(">> Caso " + nuevoTicket.getId() + " del estudiante " + nuevoTicket.getEstudiante() + " recibido y encolado. <<");
+    }
+
+    public static Ticket atenderSiguienteCaso(){
+        if(ticketQueue.isEmpty()){
+            System.out.println("No hay casos en espera.");
+            return null;
+        }
+        Ticket siguienteTicket = ticketQueue.peek();
+        siguienteTicket.setEstado(EstadoTicket.EN_ATENCION);
+        System.out.println(">> Atendiendo caso " + siguienteTicket.getId() + " del estudiante " + siguienteTicket.getEstudiante() + " <<");
+        return siguienteTicket;
+        //enviar a otro menu de atencion
+    }
+
+    /*public static void verCasosEnEspera(){
+        System.out.println("--- Casos en Espera (" + ticketQueue.size() + ") ---");
+        if(ticketQueue.isEmpty()){
+            System.out.println("No hay casos en espera.");
+            return;
+        }
+        Ticket current = ticketQueue.peek();
+        System.out.print("Frente -> ");
+        while(current != null){
+            System.out.print("[ID " + current.getId() + "] -> ");
+            current = ;
+        }
+        System.out.println("Fin");
+    }*/
 }
--- src/dominio/Ticket.java
+++ src/dominio/Ticket.java
@@ -4,12 +4,15 @@ import estructuras.SimpleList;
 
 public class Ticket {
     private int id;
+    private String estudiante;
+    private String tipoTramite;
     private EstadoTicket estado;
     private SimpleList<Nota> historialNotas;
 
-    public Ticket(int id, EstadoTicket estado) {
-        this.id = id;
-        this.estado = estado;
+    public Ticket(String estudiante, String tipoTramite ) {
+        this.estudiante = estudiante;
+        this.tipoTramite = tipoTramite;
+        this.estado = EstadoTicket.EN_COLA;
         this.historialNotas = new SimpleList<>();
     }
 
@@ -35,6 +38,22 @@ public class Ticket {
 
     public void setHistorialNotas(SimpleList<Nota> historialNotas) {
         this.historialNotas = historialNotas;
+    }
+
+    public String getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(String estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public String getTipoTramite() {
+        return tipoTramite;
+    }
+
+    public void setTipoTramite(String tipoTramite) {
+        this.tipoTramite = tipoTramite;
     }
 
     public Nota agregarNota(String observacion) {
